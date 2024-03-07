8ba66a961372ba8806a092f0b3ad4cf1b555cbbf
Kauancpi <kauancpi@gmail.com>
2024-03-06 21:32:00 -0300

    amogus

--- imepp/homework1/F.cpp
+++ imepp/homework1/F.cpp
@@ -15,32 +15,66 @@ int main(){
 
     cin >> nome_problema;
 
-    bool isFound1 = nome_problema.find(amigo1) != string::npos;
-    bool isFound2 = nome_problema.find(amigo2) != string::npos;
-    bool isFound3 = nome_problema.find(amigo3) != string::npos;
-    bool isFound4 = nome_problema.find(amigo4) != string::npos;
-    bool isFound5 = nome_problema.find(amigo5) != string::npos;
+    size_t isFound1 = nome_problema.find(amigo1);
+    size_t isFound2 = nome_problema.find(amigo2);
+    size_t isFound3 = nome_problema.find(amigo3);
+    size_t isFound4 = nome_problema.find(amigo4);
+    size_t isFound5 = nome_problema.find(amigo5);
+    if (isFound1 != string ::npos)
+    {
+        k++;
+    }
+    char arr1[]="Danil";
+    isFound1=nome_problema.find(arr1, isFound1+1);
+    if (isFound1 != string::npos)
+    {
+        k++;
+    }
 
-    if (isFound1)
+    if (isFound2!=string::npos)
     {
         k++;
     }
-    if (isFound2)
+    char arr2[]="Olya";
+    isFound2=nome_problema.find(arr2, isFound2+1);
+    if (isFound2 != string::npos)
     {
         k++;
     }
-    if (isFound3)
+
+    if (isFound3!=string::npos)
     {
         k++;
     }
-    if (isFound4)
+    char arr3[]="Slava";
+    isFound3=nome_problema.find(arr3, isFound3+1);
+    if (isFound3 != string::npos)
     {
         k++;
     }
-    if (isFound5)
+
+    if (isFound4!=string::npos)
     {
         k++;
     }
+    char arr4[]="Ann";
+    isFound4=nome_problema.find(arr4, isFound4+1);
+    if (isFound4!=string::npos)
+    {
+        k++;
+    }
+
+    if (isFound5!=string::npos)
+    {
+        k++;
+    }
+    char arr5[]="Nikita";
+    isFound5=nome_problema.find(arr5, isFound5+1);
+    if (isFound5!=string::npos)
+    {
+        k++;
+    }
+    
 
     if(k==1){
         cout << "YES";
