adeee0adab00a185632f137082ae8fd2654c377f
Kauancpi <kauancpi@gmail.com>
2024-05-17 13:54:20 -0300

    a

---
+++ teste.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Soldado{
+    protected:
+    string nome;
+    int saude;
+    int poder_de_ataque;
+
+    public:
+    Soldado(string nome_personagem,int saude_valor,int ataque):nome(nome_personagem),saude(saude_valor),poder_de_ataque(ataque){}
+    
+};
