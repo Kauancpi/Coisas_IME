2c88ed546e5148345f0834a99b8b57f089947193
Kauancpi <kauancpi@gmail.com>
2024-05-22 11:26:53 -0300

    true

---
+++ lab_comp/questoes_aleatorias_em_sala/contar_objetos.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+class contador{
+    static int counter;
+    public:
+    contador(){
+        counter ++;
+    }
+
+    static void show(){
+        cout << counter << endl;
+    }
+
+    ~contador(){
+        counter --;
+    }
+
+};
+
+int contador::counter=0;
+
+int main(){
+    contador *x1;
+    contador *x2;
+    contador *x3;
+    contador *x4;
+
+    x1=new contador;
+    x2=new contador;
+    x3=new contador;
+    x4=new contador;
+
+
+    x1->show();
+
+    delete x1;
+
+    contador::show();
+
+    delete x2;
+    delete x3;
+    delete x4;
+
+    contador::show();
+
+    
+}
