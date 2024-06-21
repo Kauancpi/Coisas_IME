5eba846e6abe1b9374f370baef8151b583c38881
Kauancpi <kauancpi@gmail.com>
2024-06-20 21:39:59 -0300

    a

--- lab_comp/trabalho/lig4_real.cpp
+++ lab_comp/trabalho/lig4_real.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <memory>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,16 +10,18 @@ class lig4{
     int linhas;
     int colunas;
     int vez_jogador;
+    
+
    
     public:
-    lig4(int numero_linhas,int numero_colunas):vez_jogador(1),linhas(numero_linhas),colunas(numero_colunas){
-        pointer= new char*[numero_linhas];
+    lig4():vez_jogador(1),linhas(6),colunas(7){
+        pointer= new char*[6];
 
-        for(int i=0;i<numero_linhas;i++){
-            pointer[i]=new char[numero_colunas];
+        for(int i=0;i<6;i++){
+            pointer[i]=new char[7];
         }
-        for(int i=0;i<numero_linhas;i++){
-            for(int j=0;j<numero_colunas;j++){
+        for(int i=0;i<6;i++){
+            for(int j=0;j<7;j++){
                 pointer[i][j]='.';
             }
         }
@@ -176,6 +181,8 @@ class lig4{
         
         char c='s';
         int k;
+        int vitorias_1=0;
+        int vitorias_2=0;
         while(c=='s'||c=='S'){
             for(int i=0;i<linhas;i++){
                 for(int j=0;j<colunas;j++){
@@ -186,34 +193,283 @@ class lig4{
             while(!game_over()){
                 cout << "Vez do jogador " << vez_jogador << ":" << endl;
                 imprimir();
-
+                if(vez_jogador==1){
+                    k=melhor_jogada_cope()+1;
+                    while(!verificar(k)){
+                        k=rand()%7;
+                        k+1;
+                    }
+                }
+                else if(vez_jogador==2){
                 do{
-                    cout << "Em qual coluna vc gostaria de jogar? (1- " << colunas << ")" << endl;
-                    cin >> k;
-                } while(!verificar(k));
+                    k=rand()%7+1;
+                } while(!verificar(k));}
 
                 jogar(k);
                 alternar_vez();
             }
             imprimir();
             pair<int,int> a = resultado_final();
-            if(a.first==1){
+            if(a.first>0){
                 cout << "jogador 1 ganhou" << endl;
+                vitorias_1++;
             }
-            else if(a.second==1){
+            else if(a.second>0){
                 cout << "jogador 2 ganhou" << endl;
+                vitorias_2++;
             }
             else{
                 cout << "empate" << endl;
             }
-            cout << "Gostaria de jogar novamente? (s/n)" << endl;
-            cin >> c; 
+
+            if(vitorias_2>0){
+                c='f';
+            }
+            
         }
+
+        cout << "bot ganhou:" << vitorias_1 << endl;
+
+    }
+
+    int melhor_jogada_cope(){
+        pair<int,int> omegalul;
+        int k=vez_jogador;
+        long long int x[7]={0,0,0,0,0,0,0};
+        char*** xdd;
+        xdd=new char**[7*7*7*7*7];
+
+        for(int i=0;i<7*7*7*7*7;i++){
+            xdd[i]=new char*[6];
+        }
+
+        for(int i=0;i<7*7*7*7*7;i++){
+            for(int j=0;j<6;j++){
+                xdd[i][j]=new char[7];
+            }
+        }
+
+        for(int i=0;i<7*7*7*7*7;i++){
+            for(int j=0;j<6;j++){
+                for(int k=0;k<7;k++){
+                    xdd[i][j][k]=pointer[j][k];
+                }
+            }
+        }
+
+        
+        for(int a=0;a<7;a++){
+            for(int b=0;b<7;b++){
+                for(int c=0;c<7;c++){
+                    for(int d=0;d<7;d++){
+                        for(int e=0;e<7;e++){
+                            k=1;
+
+                            if(!verificar_qualquer(a+1,xdd[a*7*7*7*7+b*7*7*7+c*7*7+d*7+e])){
+                                continue;
+                            }
+                            jogar_qualquer_jogo(a+1,xdd[a*7*7*7*7+b*7*7*7+c*7*7+d*7+e],k);
+
+                            k=2;
+
+                            omegalul=resultado_final_qualquer_jogo(xdd[a*7*7*7*7+b*7*7*7+c*7*7+d*7+e]);
+                            if(omegalul.second>0){
+                                x[a]-=10;
+                            }
+                            if(omegalul.first>0){
+                                x[a]+=1;
+                            }
+
+                            if(!verificar_qualquer(b+1,xdd[a*7*7*7*7+b*7*7*7+c*7*7+d*7+e])){
+                                continue;
+                            }
+                            jogar_qualquer_jogo(b+1,xdd[a*7*7*7*7+b*7*7*7+c*7*7+d*7+e],k);
+
+                            k=1;
+
+                            omegalul=resultado_final_qualquer_jogo(xdd[a*7*7*7*7+b*7*7*7+c*7*7+d*7+e]);
+                            if(omegalul.second>0){
+                                x[a]-=10;
+                            }
+                            if(omegalul.first>0){
+                                x[a]+=1;
+                            }
+
+                            if(!verificar_qualquer(c+1,xdd[a*7*7*7*7+b*7*7*7+c*7*7+d*7+e])){
+                                continue;
+                            }
+                            jogar_qualquer_jogo(c+1,xdd[a*7*7*7*7+b*7*7*7+c*7*7+d*7+e],k);
+
+                            k=2;
+
+                            omegalul=resultado_final_qualquer_jogo(xdd[a*7*7*7*7+b*7*7*7+c*7*7+d*7+e]);
+                            if(omegalul.second>0){
+                                x[a]-=10;
+                            }
+                            if(omegalul.first>0){
+                                x[a]+=1;
+                            }
+
+                            if(!verificar_qualquer(d+1,xdd[a*7*7*7*7+b*7*7*7+c*7*7+d*7+e])){
+                                continue;
+                            }
+                            jogar_qualquer_jogo(d+1,xdd[a*7*7*7*7+b*7*7*7+c*7*7+d*7+e],k);
+
+                            k=1;
+
+                            omegalul=resultado_final_qualquer_jogo(xdd[a*7*7*7*7+b*7*7*7+c*7*7+d*7+e]);
+                            if(omegalul.second>0){
+                                x[a]-=10;
+                            }
+                            if(omegalul.first>0){
+                                x[a]+=1;
+                            }
+
+                            if(!verificar_qualquer(e+1,xdd[a*7*7*7*7+b*7*7*7+c*7*7+d*7+e])){
+                                continue;
+                            }
+                            jogar_qualquer_jogo(e+1,xdd[a*7*7*7*7+b*7*7*7+c*7*7+d*7+e],k);
+
+                            omegalul=resultado_final_qualquer_jogo(xdd[a*7*7*7*7+b*7*7*7+c*7*7+d*7+e]);
+                            if(omegalul.second>0){
+                                x[a]-=10;
+                            }
+                            if(omegalul.first>0){
+                                x[a]+=1;
+                            }
+
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+        int h=0;
+        for(int i=1;i<7;i++){
+            if(x[i]>x[h]){
+                h=i;
+            }
+        }
+
+        for(int i=0;i<7*7*7*7*7;i++){
+            for(int j=0;j<6;j++){
+                delete[] xdd[i][j];
+            }
+        }
+
+        for(int i=0;i<7*7*7*7*7;i++){
+            delete[] xdd[i];
+        }
+
+        delete[] xdd;
+
+
+
+
+        return(h);
+
+
+    }
+
+    bool jogar_qualquer_jogo(int coluna,char **pointer,int vez_jogador){
+        if(verificar_qualquer(coluna,pointer)){
+            if(vez_jogador==1){
+                for(int i=linhas-1;i>=0;i--){
+                    if(pointer[i][colunas-coluna]=='.'){
+                        pointer[i][colunas-coluna]='X';
+                        return(true);
+                    }
+                }
+            }
+            else if(vez_jogador==2){
+                for(int i=linhas-1;i>=0;i--){
+                    if(pointer[i][colunas-coluna]=='.'){
+                        pointer[i][colunas-coluna]='O';
+                        return(true);
+                    }
+                }
+            }
+        }
+        return(false);
+    }
+
+    pair<int,int> resultado_final_qualquer_jogo(char **pointer){
+        int pontos1=0;
+        int pontos2=0;
+        pair<int,int> xdd;
+
+        for(int i=0;i<linhas;i++){
+            for(int j=0;j<colunas;j++){
+                if(pointer[i][j]=='X'){
+                    if(j+3<colunas){
+                        if(pointer[i][j+1]=='X'&&pointer[i][j+2]=='X'&&pointer[i][j+3]=='X'){
+                            pontos1++;
+                        }
+                    }
+                    if(i+3<linhas){
+                        if(pointer[i+1][j]=='X' && pointer[i+2][j]=='X' && pointer[i+3][j]=='X'){
+                            pontos1++;
+                        }
+                    }
+                    if(i+3<linhas && j+3< colunas){
+                        if(pointer[i+1][j+1]=='X' && pointer[i+2][j+2]=='X' && pointer[i+3][j+3]=='X'){
+                            pontos1++;
+                        }
+                    }
+                    if(i+3<linhas && j-3>=0){
+                        if(pointer[i+1][j-1]=='X' && pointer[i+2][j-2]=='X' && pointer[i+3][j-3]=='X'){
+                            pontos1++;
+                        }
+                    }
+
+                }
+                else if(pointer[i][j]=='O'){
+                    if(j+3<colunas){
+                        if(pointer[i][j+1]=='O'&&pointer[i][j+2]=='O'&&pointer[i][j+3]=='O'){
+                            pontos2++;
+                        }
+                    }
+                    if(i+3<linhas){
+                        if(pointer[i+1][j]=='O' && pointer[i+2][j]=='O' && pointer[i+3][j]=='O'){
+                            pontos2++;
+                        }
+                    }
+                    if(i+3<linhas && j+3< colunas){
+                        if(pointer[i+1][j+1]=='O' && pointer[i+2][j+2]=='O' && pointer[i+3][j+3]=='O'){
+                            pontos2++;
+                        }
+                    }
+                    if(i+3<linhas && j-3>=0){
+                        if(pointer[i+1][j-1]=='O' && pointer[i+2][j-2]=='O' && pointer[i+3][j-3]=='O'){
+                            pontos2++;
+                        }
+                    }
+
+                }
+
+            }
+        }
+        xdd.first=pontos1;
+        xdd.second=pontos2;
+
+        return(xdd);
+    }
+
+    bool verificar_qualquer(int coluna,char **pointer){
+        if(coluna>colunas || coluna<1){
+            return(false);
+        }
+        for(int i=0;i<linhas;i++){
+            if(pointer[i][colunas-coluna]=='.'){
+                return(true);
+            }
+        }
+        return(false);
 
     }
     
-
-
 
     ~lig4(){
         for(int i=0;i<linhas;i++){
@@ -221,21 +477,15 @@ class lig4{
         }
         delete[] pointer;
     }
+
+
 };
 
 
 
 
-int main(){
-    int linhas;
-    int colunas;
-    
-    cout << "Coloque o numero de linhas: " << endl;
-    cin >> linhas;
 
-    cout << "Coloque o numero de colunas: " <<endl;
-    cin >> colunas;
-    
-    lig4 jogo(linhas,colunas);
+int main(){
+    lig4 jogo;
     jogo.jogar_jogo();
 }
